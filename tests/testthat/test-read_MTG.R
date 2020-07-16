ac6308cfeea23f3c696db96b51ffd0a6485e9316
remi.vezy <VEZY@users.noreply.github.com>
2020-07-16 14:55:20 +0200

    Update test-read_MTG.R

--- tests/testthat/test-read_MTG.R
+++ tests/testthat/test-read_MTG.R
@@ -1,7 +1,7 @@
 context("Read MTG")
 
 filepath= system.file("extdata", "simple_plant.mtg", package = "XploRer")
-MTG_file = readLines(file)
+MTG_file = readLines(filepath)
 MTG_file = strip_comments(MTG_file)
 MTG_file = strip_empty_lines(MTG_file)
 
@@ -9,25 +9,40 @@ test_that("Check the sections", {
   expect_null(check_sections(MTG_file))
 })
 
-test_that("Read code", {
-  expect_equal(read_MTG_code(MTG_file), "FORM-A")
+test_that("Parse code", {
+  expect_equal(parse_MTG_code(MTG_file), "FORM-A")
 })
 
-test_that("Read classes", {
-  classes = read_MTG_classes(MTG_file)
+classes = parse_MTG_classes(MTG_file)
+
+test_that("Parse classes", {
   expect_true(is.data.frame(classes))
   expect_equal(nrow(classes),5)
   expect_known_hash(classes, hash = "d8df027e91")
 })
 
-test_that("Read description", {
-  description = read_MTG_description(MTG_file)
+description = parse_MTG_description(MTG_file)
+
+test_that("Parse description", {
   expect_known_hash(description, hash = "2ab85874bd")
 })
 
-test_that("Read features", {
-  features = read_MTG_section(MTG_file,"FEATURES:",
-                              c("NAME", "TYPE"),
-                              "MTG:",TRUE)
+features = parse_MTG_section(MTG_file,"FEATURES:",
+                             c("NAME", "TYPE"),
+                             "MTG:",TRUE)
+
+test_that("Parse features", {
   expect_known_hash(features, hash = "d2f93f8d5c")
 })
+
+test_that("Parse MTG", {
+  MTG = parse_MTG_MTG(MTG_file,classes,description,features)
+  expect_known_hash(MTG, hash = "33f0328ef1")
+})
+
+test_that("Read MTG file", {
+  MTG = read_MTG(filepath)
+  expect_known_hash(MTG, hash = "c8782da263")
+})
+
+
